dbed763f0497ba4e8d9deca7610166a0f22e7c16
MorskiBG <dymityrp@yahoo.com>
2021-09-22 16:31:11 +0300

    test

---
+++ cubical/app/config/env_config.js
@@ -0,0 +1,10 @@
+const config = {
+    development: {
+        port: process.env.PORT || 3000,
+        // host: process.env.HOST || 'localhost',
+
+    },
+    production: {}
+};
+
+module.exports = config;
